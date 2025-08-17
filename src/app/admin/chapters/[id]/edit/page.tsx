f818ea1ce66a9d8e7523b30b2ecb04d8c4e721cf
YaseiTanuki <tanuki1707@gmail.com>
2025-08-17 15:11:02 +0700

    fix white bg

--- src/app/admin/chapters/[id]/edit/page.tsx
+++ src/app/admin/chapters/[id]/edit/page.tsx
@@ -129,7 +129,7 @@ export default function EditChapterPage() {
   };
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-black">
       <Navigation />
       
       {/* Page Title */}
